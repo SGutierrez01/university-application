a51b50676a6b71fdc296018d55fba0abe39abb11
Santiago Gutierrez Villegas <santiago.gutierrez01@globant.com>
2024-08-13 00:32:21 -0500

    Feat: Build models of system.

--- src/models/Person.java
+++ src/models/Person.java
@@ -1,4 +1,32 @@
-package main.java.com.university.models;
+package models;
 
-public class Person {
+public abstract class Person {
+    private int personId;
+    private String personFirstname;
+    private String personLastname;
+    private int personAge;
+
+    public Person(int personId, String personFirstname, String personLastname, int personAge) {
+        this.personId = personId;
+        this.personFirstname = personFirstname;
+        this.personLastname = personLastname;
+        this.personAge = personAge;
+    }
+
+    public int getId() {
+        return personId;
+    }
+
+    public String getFirstname() {
+        return personFirstname;
+    }
+
+    public String getLastname() {
+        return personLastname;
+    }
+
+    public int getAge() {
+        return personAge;
+    }
 }
+
--- src/models/Student.java
+++ src/models/Student.java
@@ -1,4 +1,15 @@
-package main.java.com.university.models;
+package models;
 
-public class Student {
+public class Student extends Person {
+    private String studentId;
+
+    public Student(int personId, String personFirstname, String personLastname, int personAge, String studentId) {
+        super(personId, personFirstname, personLastname, personAge);
+        this.studentId = studentId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
 }
+
